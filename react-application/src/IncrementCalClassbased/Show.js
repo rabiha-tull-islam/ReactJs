2926b8904121fb171158e00ce6d778c568c57355
rabiha-tull-islam <rabihaislam2@gmail.com>
2021-09-02 12:40:24 +0500

    incrementCalculatorClassBased

---
+++ react-application/src/IncrementCalClassbased/Show.js
@@ -0,0 +1,26 @@
+// import React, { Component } from 'react'
+const Show=({number})=>{
+    return(
+        <div>
+            <div>
+            number is: {number}
+            </div>
+          
+        </div>
+    )
+}
+export default Show;
+// export class Show extends Component{
+//     render(){
+//         return(
+//             <div>
+//                 <div>
+//                 number is: {this.state.number}
+//                 </div>
+              
+//             </div>
+//         )
+//     }
+  
+
+// }
