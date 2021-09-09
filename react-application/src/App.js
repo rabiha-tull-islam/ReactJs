1769b16a76d174f9a8417e640a60aa0d5c868a36
rabiha-tull-islam <rabihaislam2@gmail.com>
2021-09-09 13:55:46 +0500

    Main

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -1,11 +1,11 @@
-// import logo from './logo.svg';
 import './App.css';
-import Card from './Tasks/CardAssingnment-4'
 
 
 function App() {
   return (
-    <Card/>
+    <div>
+      Hello World
+    </div>
   );
 }
 
