4cc80ddecf0f5a0adef57b6267fb89de14d01901
rabiha-tull-islam <rabihaislam2@gmail.com>
2021-09-22 19:25:31 +0500

    done

--- react-application/src/Tasks/Accordion/Accordion.js
+++ react-application/src/Tasks/Accordion/Accordion.js
@@ -1,48 +1,60 @@
-import React,{useState} from 'react';
-import './Accordion.css';
-import Data from './Data';
+import React, { useState } from "react";
+import "./Accordion.css";
+import Data from "./Data";
 // import AccorionItems from './AccordionItems';
-const Accordion=()=>{
-    let i=null
-    const [show,setshow]=useState(i)
-    const contentData=Data.map((Data,i)=>Data.Answer)
-    // console.log(contentData)
-    const content= new Set(contentData);
-    // console.log(content);
-    const contentArray = [...content];
-    const contentString= contentArray.toString();
-    const contentSlice = contentString.slice(0,199)
-    // console.log(contentSlice)
-    const [readmore, setreadmore]=useState(true)
-    const setContent= readmore? contentSlice:contentString;
-    const toggle=(tog)=>{
-       
-        if(show===tog)
-        {
-            return setshow(null)
-        }
-        setshow(tog);
+const Accordion = () => {
+  let i = null;
+  const [show, setshow] = useState(i);
+  const contentData = Data.map((Data, i) => Data.Answer);
+  // console.log(contentData)
+  const content = new Set(contentData);
+  // console.log(content);
+  const contentArray = [...content];
+  const contentString = contentArray.toString();
+  const contentSlice = contentString.slice(0, 199);
+  // console.log(contentSlice)
+  const [readmore, setreadmore] = useState(true);
+  const setContent = readmore ? contentSlice : contentString;
+  const toggle = (tog) => {
+    if (show === tog) {
+      return setshow(null);
     }
-    
-    return(
-         <div className="Box">
-            <div className="Accordion">
-             {Data.map((items,i)=> <div  className="Item" key={i}>
-                  <div className="title">
-                      <div>
-                          <span>Quesition # {items.id}</span>   
-                          {items.Question}
-                      </div>
-                      <span className="toggle" onClick={()=>toggle(i)}>{show===i? <i className="fas fa-chevron-down"></i>:<i className="fas fa-chevron-right"></i>}</span>
-                  </div>
-                   <div className={show===i? 'contentShow ':'content '}>
-                    {setContent}.....<a href="#" className={show===i ? 'ashow':'ahidden'}  onClick={()=>setreadmore(!readmore)}>{readmore? 'Read more': 'Read less'}</a>
-                   </div>
-    </div>
-       )}
-            </div>
-        </div>
-    )
+    setshow(tog);
+  };
 
-}
+  return (
+    <div className="Box">
+      <div className="Accordion">
+        {Data.map((items, i) => (
+          <div className="Item" key={i}>
+            <div className="title">
+              <div>
+                <span>Quesition # {items.id}</span>
+                {items.Question}
+              </div>
+              <span className="toggle" onClick={() => toggle(i)}>
+                {show === i ? (
+                  <i className="fas fa-chevron-down"></i>
+                ) : (
+                  <i className="fas fa-chevron-right"></i>
+                )}
+              </span>
+            </div>
+            <div className={show === i ? "contentShow " : "content "}>
+              {setContent}
+              {readmore ? "...." : " "}
+              <a
+                href="#"
+                className={show === i ? "ashow" : "ahidden"}
+                onClick={() => setreadmore(!readmore)}
+              >
+                {readmore ? "Read More" : "Read Less"}
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Accordion;
